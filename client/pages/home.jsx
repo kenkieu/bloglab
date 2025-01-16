3747306b93fc1689e4a6447087e07259dc6e5031
Kenneth Kieu <kenkieu.dev@gmail.com>
2025-01-16 00:20:52 -0800

    fix mobile

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import NoResults from './no-results';
-import ConnectionError from './connection-error';
+import React from "react";
+import NoResults from "./no-results";
+import ConnectionError from "./connection-error";
 
 class Home extends React.Component {
   constructor(props) {
@@ -10,7 +10,7 @@ class Home extends React.Component {
       userPosts: [],
       showUserPosts: false,
       loading: false,
-      error: false
+      error: false,
     };
     this.togglePosts = this.togglePosts.bind(this);
   }
@@ -18,31 +18,31 @@ class Home extends React.Component {
   componentDidMount() {
     this.setState({ loading: true });
     this.setState({ error: false });
-    fetch('/api/posts')
-      .then(res => res.json())
-      .then(posts => {
+    fetch("/api/posts")
+      .then((res) => res.json())
+      .then((posts) => {
         this.setState({ posts });
         this.setState({ loading: false });
       })
-      .catch(err => {
+      .catch((err) => {
         this.setState({ loading: false });
         this.setState({ error: true });
         console.error(err);
       });
 
-    const jwt = localStorage.getItem('jwt-token');
+    const jwt = localStorage.getItem("jwt-token");
     const req = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-access-token': jwt
-      }
+        "x-access-token": jwt,
+      },
     };
-    fetch('/api/my-posts', req)
-      .then(res => res.json())
-      .then(userPosts => {
+    fetch("/api/my-posts", req)
+      .then((res) => res.json())
+      .then((userPosts) => {
         this.setState({ userPosts });
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   }
 
   togglePosts() {
@@ -51,13 +51,12 @@ class Home extends React.Component {
 
   render() {
     const { showUserPosts, loading, error } = this.state;
-    const btnText = showUserPosts === false ? 'My Posts' : 'Your Feed';
-    const headerText = showUserPosts === false ? 'Your Feed' : 'My Posts';
+    const btnText = showUserPosts === false ? "My Posts" : "Your Feed";
+    const headerText = showUserPosts === false ? "Your Feed" : "My Posts";
 
     return (
       <>
-        {loading
-          ? (
+        {loading ? (
           <div className="container">
             <div className="row">
               <div className="col s12 l12">
@@ -67,12 +66,9 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-            )
-          : error
-            ? (
+        ) : error ? (
           <ConnectionError />
-              )
-            : (
+        ) : (
           <>
             <div className="container feed flex-center flex-wrap">
               <div className="row width-100">
@@ -82,11 +78,9 @@ class Home extends React.Component {
               </div>
             </div>
             <div className="container">
-              {!this.state.posts.length
-                ? (
+              {!this.state.posts.length ? (
                 <NoResults user={this.props.user} />
-                  )
-                : (
+              ) : (
                 <>
                   {this.props.user && (
                     <div className="row width-100 mb-two-rem">
@@ -96,7 +90,9 @@ class Home extends React.Component {
                           className="btn-large blue darken-2 width-100 waves-effect waves-light align-center flex-center gap-half"
                         >
                           NEW POST
-                          <i className="material-icons">add</i>
+                          <span className="home-cta">
+                            <i className="material-icons">add</i>
+                          </span>
                         </a>
                       </div>
                       <div className="col s6 s6">
@@ -105,29 +101,31 @@ class Home extends React.Component {
                           className="btn-large grey darken-4 width-100 waves-effect waves-light align-center flex-center gap-half"
                         >
                           {btnText}
-                          <i className="material-icons">library_books</i>
+                          <span className="home-cta">
+                            <i className="material-icons">library_books</i>
+                          </span>
                         </a>
                       </div>
                     </div>
                   )}
                   <div className="row flex-wrap">
                     {!this.state.showUserPosts
-                      ? this.state.posts.map(post => (
+                      ? this.state.posts.map((post) => (
                           <div key={post.postId} className="col s12 l6 m-0">
                             <Post post={post} />
                           </div>
-                      ))
-                      : this.state.userPosts.map(userPost => (
+                        ))
+                      : this.state.userPosts.map((userPost) => (
                           <div key={userPost.postId} className="col s12 l6 m-0">
                             <MyPost userPost={userPost} />
                           </div>
-                      ))}
+                        ))}
                   </div>
                 </>
-                  )}
+              )}
             </div>
           </>
-              )}
+        )}
       </>
     );
   }
